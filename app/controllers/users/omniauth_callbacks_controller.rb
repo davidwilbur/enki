e0bbba6207a426c2e0d42bdb49b99888c630ee3c
David Wilbur <davidwilbur1@gmail.com>
2014-05-17 13:45:33 -0400

    Fixed FB button on signup and signin pages.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      flash[:success] = "Successfully signed in with Facebook."
+      #set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to sign_up_path
